67816899cd488a75a364730b8bb964829fd3d9b5
Sayginsaracoglu <93162838+Sayginsaracoglu@users.noreply.github.com>
2023-04-18 21:55:12 -0400

    performance 1.1

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,9 @@
 "use client";
 import ProjectCard from "@/components/ProjectCard";
 import { Row, Col, Card, Button } from "react-bootstrap";
+import Link from "next/link";
 import projects from "@/data/projectsData";
+import { useEffect, useState } from "react";
 
 interface Technology {
   name: string;
@@ -11,6 +13,17 @@ interface Technology {
 const emptyTech: Technology = { name: "", imgSrc: "" };
 
 export default function Projects() {
+  const [isMobile, setMobile] = useState(window.innerWidth < 600);
+
+  const updateMedia = () => {
+    setMobile(window.innerWidth < 600);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <>
       <br />
@@ -25,26 +38,58 @@ export default function Projects() {
           (project) =>
             project.active && (
               <Col lg={6} key={project.id}>
-                {/* <ProjectCard
-                  title={project.title}
-                  description={project.description}
-                  imageSrc={project.imageSrc}
-                  sourceUrl={project.sourceUrl}
-                  url={project.url ? project.url : undefined}
-                  usedTech={project.usedTech ? project.usedTech : undefined}
-                /> */}
-                <Card style={{ width: "auto", borderRadius: "10px" }}>
-                  <Card.Img
-                    style={{ objectFit: "cover", height: "12rem" }}
-                    variant="top"
-                    src={project.imageSrc[0]}
+                {isMobile ? (
+                  <Card
+                    style={{
+                      width: "auto",
+                      borderRadius: "10px",
+                      backgroundColor: "#0f111466",
+                    }}
+                  >
+                    {project.url ? (
+                      <Link href={project.url}>
+                        <Card.Img
+                          style={{
+                            objectFit: "cover",
+                            height: "12rem",
+                            boxShadow: "0px 10px 10px black",
+                          }}
+                          variant="top"
+                          src={project.imageSrc[0]}
+                        />
+                      </Link>
+                    ) : (
+                      <Card.Img
+                        style={{
+                          objectFit: "cover",
+                          height: "12rem",
+                          boxShadow: "0px 10px 10px black",
+                        }}
+                        variant="top"
+                        src={project.imageSrc[0]}
+                      />
+                    )}
+
+                    <Card.Body>
+                      <Card.Title style={{ color: "white" }}>
+                        {project.title}
+                      </Card.Title>
+                      <Card.Text style={{ color: "white" }}>
+                        {project.description}
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                ) : (
+                  <ProjectCard
+                    title={project.title}
+                    description={project.description}
+                    imageSrc={project.imageSrc}
+                    sourceUrl={project.sourceUrl}
+                    url={project.url ? project.url : undefined}
+                    usedTech={project.usedTech ? project.usedTech : undefined}
                   />
-                  <Card.Body>
-                    <Card.Title>{project.title}</Card.Title>
-                    <Card.Text>{project.description}</Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                )}
               </Col>
             )
         )}
